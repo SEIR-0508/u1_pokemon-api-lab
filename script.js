86a971dd21a1d0854413f4618630b37260a182fb
ashfilbrun <ashesclaire@gmail.com>
2023-05-17 08:13:44 -0700

    pokemon pics and align css

--- script.js
+++ script.js
@@ -8,6 +8,9 @@ button.addEventListener('click', async () => {
 let pokemonName = document.querySelector("#pokemonName")
 let pokemonImage = document.querySelector("#pokemonImage")
 let pokemonImage2 = document.querySelector("#pokemonImage2")
+let heldItems = document.querySelector('#heldItems')
+let pokemonMoves = document.querySelector('#pokemonMoves')
+// let pokemonInfo = document.querySelector('.pokemonInfo')
 //where does this need to be scoped?
 let textInput = document.querySelector("#inputBar").value
 //Axios call goes here
@@ -21,10 +24,11 @@ console.log(response.data)
 pokemonName.innerText = response.data.name
 pokemonImage.src = response.data.sprites.front_default
 pokemonImage2.src = response.data.sprites.back_default
-// pokemonImage.src = pokemonImage
-
+// pokemonInfo.src = response.data.abilities
+heldItems.innerText = response.data.held_items[0].item.name
+pokemonMoves.innerText = response.data.moves[0].move.name
 //DOM Setters go here
-
+console.log(response.data.held_items[0].item.name)
 })
 
 
