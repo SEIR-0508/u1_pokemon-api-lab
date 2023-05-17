d6a8473ef77b6366db4b60c0f5a0c1b6f7b8f889
Andrew <mullinsa428@gmail.com>
2023-05-16 22:14:55 -0400

    finished getting additional attributes from the api. Will now work on adding functionality to the buttons and storing the sprites on the right.

--- script.js
+++ script.js
@@ -4,16 +4,16 @@ const catchBtn = document.querySelector("#searchButton");
 const pokemonName = document.querySelector("#pokemonName");
 const pokemonImage = document.querySelector("#pokemonImage");
 
-const releaseBtn = document.getElementById('release');
-const chooseBtn = document.getElementById('chooseYou');
-
 const pokeNum = document.getElementById('pokeNum');
 const pokeType = document.getElementById('pokeType');
 const pokeHeight = document.getElementById('pokeHeight');
-const pokeweight = document.getElementById('pokeWeight');
-const pokeItems = document.getElementById('pokeItems');
+const pokeWeight = document.getElementById('pokeWeight');
+const pokeAbil = document.getElementById('pokeAbil');
 
-const balls = document.querySelectorAll('#pokeBalls > img');
+const releaseBtn = document.getElementById('release');
+const chooseBtn = document.getElementById('chooseYou');
+
+const pokeBalls = document.querySelectorAll('#pokeBalls > img');
 
 //// EVENT LISTENERS ////
 
@@ -26,19 +26,32 @@ catchBtn.addEventListener('click', () => pokeCatch());
 async function pokeCatch() {
     {
         let textInput = document.querySelector("#inputBar").value;
-       
-    
-        //Axios call goes here
-        //remember to use Async and Await!
+
         let response  = await axios.get(`https://pokeapi.co/api/v2/pokemon/${textInput}`);
         console.log(response);
         let pokePic = response.data.sprites.front_default;
         let pokeName = response.data.forms[0].name;
+        let number = response.data.id;
+        let weight = response.data.weight;
+        let height = response.data.height;
+
+        //ability and type take a little more thought//
+        let type = response.data.types.length > 1 ? `${response.data.types[0].type.name}, ${response.data.types[1].type.name}` : response.data.types[0].type.name;
+        let abil = ''
+        for (i=0; i<response.data.abilities.length; i++) {
+            abil = i === (response.data.abilities.length - 1) ? abil += response.data.abilities[i].ability.name : abil += `${response.data.abilities[i].ability.name}, `;
+        }
+
         //DOM Setters go here
         console.log(pokePic);
         console.log(pokeName);
-        pokemonName.innerHTML = `<span>${pokeName}</span>`;
+        pokemonName.innerHTML = `${pokeName}`;
         pokemonImage.src = pokePic;
-        
+        pokeNum.innerHTML = `Num: ${number}`;
+        pokeHeight.innerHTML = `Height: ${height}`;
+        pokeWeight.innerHTML = `Weight: ${weight}`;
+        pokeType.innerHTML = `Type: ${type}`
+        pokeAbil.innerHTML = `Abilities: ${abil}`
+
     }
 }
