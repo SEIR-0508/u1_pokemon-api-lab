f133a220583ecff706cc3436fe6b78f14bc9692c
Nathania <nathania333444@gmail.com>
2023-05-17 20:45:38 -0700

    pokemon hw

--- script.js
+++ script.js
@@ -1,31 +1,27 @@
 console.log('working')
 
+let pokeName = document.querySelector("#pokemonName")
+let pokeImage = document.querySelector("#pokemonImage")
+let searchValue = document.querySelector("#inputBar")
+let button = document.querySelector("#searchButton")
+console.log()
+
+
 
 const getPokemon = async () => {
-    pokemonSpecies = await axios.get('https://dog.ceo/api/breeds/list/all')
-    console.log(dogBreeds.data.message)
+    pokemon = await axios.get('https://pokeapi.co/api/v2/pokemon/')
+   console.log(pokemon)
  }
 
- getBreeds()
-
-
-
-
-
-
-let button = document.querySelector("#searchButton")
-
+ 
 button.addEventListener('click', async () => {
+    pokemon = await axios.get(`https://pokeapi.co/api/v2/pokemon/${searchValue.value}`)
+    pokemonName.innerText = pokemon.data.name
+    pokemonImage.src = pokemon.data.sprites.front_default
+    console.log(pokemon)
 
-    let pokemonName = document.querySelector("#pokemonName")
-    let pokemonImage = document.querySelector("#pokemonImage")
-    //where does this need to be scoped?
-    let textInput = document.querySelector("#inputBar").value
-        
-
-    //Axios call goes here
-    //remember to use Async and Await!
-    //DOM Setters go here
+    
+    
 
 }
 )
