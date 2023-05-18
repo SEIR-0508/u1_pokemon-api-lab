aff6c044469de3d4fa7a839a0c548cca86cb53bc
Bryan <b.velez205@gmail.com>
2023-05-17 20:08:09 -0400

    Bryan Velez

---
+++ pokemon.js
@@ -0,0 +1,55 @@
+console.log('working')
+
+const typeInput = document.querySelector('input')
+let button = document.querySelector("#searchButton")
+let image = document.querySelector('#pokemonImage')
+
+
+
+
+button.addEventListener('click', async () => {
+
+    let displayName = document.querySelector("#pokemonName")
+    let pokemonImage = document.querySelector("#pokemonImage")
+    let pokemonWeight = document.querySelector('#weight')
+    // let pokemonAbilities = document.querySelector('#abilities')
+    let textInput = document.querySelector("#inputBar").value
+    let height = document.querySelector('#height')
+
+    let pokeType = await axios.get(`https://pokeapi.co/api/v2/pokemon/${textInput}`)
+    console.log(pokeType.data.sprites.front_default)
+    displayName.innerHTML = pokeType.data.name    
+    pokemonImage.src = pokeType.data.sprites.front_default
+    pokemonWeight.innerHTML = `Weight: ${pokeType.data.weight}`
+    height.innerHTML = `Height: ${pokeType.data.height}`
+    // pokemonAbilities.innerHTML = `Abilities: ${pokeType.data.abilities.map(abilities) => }`
+   
+   
+    console.log(pokeType)
+
+})
+
+
+
+
+// const getPokemon = async () => {
+   
+//     displayName.innerHTML = pokeType.data.name
+//     }
+
+
+
+
+// searchButton.addEventListener('click', async () => {
+//     let type = inputBar.value
+//     console.log(type)
+//     let response = await axios.get(`https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0${type}`)
+//     console.log(response)
+
+// })
+
+
+
+
+
+// getPokemon()
