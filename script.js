6a259346340da6d3f396f3f63a6a81655f04352a
Dan Mulka <danielmulka92@gmail.com>
2023-05-17 23:54:38 -0500

    pokeDex2

--- script.js
+++ script.js
@@ -11,6 +11,10 @@
 console.log(response)
 
 
+ //finds name
+ let pokemonName = document.querySelector(`#pokemonName`)
+ let pokeName = response.data.species.name 
+pokemonName.innerHTML= pokeName
 
  //finds immage 
  let pokemonImage = document.querySelector(`#pokemonImage`)
@@ -20,14 +24,14 @@ console.log(response)
 //finds type
 let pokemonType= document.querySelector('#pokemonType')
 let pokeType = response.data.types.map(type => type.type.name)
- pokemonType.innerHTML= pokeType.join(', ')
+ pokemonType.innerHTML= `Type: ${pokeType.join(', ')}`
 
  //find weight
 let pokemonWeight= document.querySelector('#pokemonWeight')
 let pokeWeight = response.data.weight
- pokemonWeight.innerHTML= pokeWeight
+ pokemonWeight.innerHTML= `Weight: ${pokeWeight} Kg`
 
- 
+ //find evolve from  - * needs fixing *
  let pokemonEvolveFrom= document.querySelector('#pokemonEvolveFrom"')
  let pokeEvolveF = response.data.evolves_from_species.name
   pokemonEvolveFrom.innerHTML= pokeEvolveF
