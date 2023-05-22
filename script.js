47dcc906e34e7dbf943a615b12c5a3164152d982
chrischen <chrischen96@outlook.com>
2023-05-22 00:45:02 -0700

    finished

--- script.js
+++ script.js
@@ -5,10 +5,11 @@ button.addEventListener('click', async () => {
 
     let pokemonName = document.querySelector("#pokemonName")
     let pokemonImage = document.querySelector("#pokemonImage")
-    //where does this need to be scoped?
+    let homeImage = document.querySelector('#homeImage')
     let textInput = document.querySelector("#inputBar").value
     let name = document.getElementById('name')
     let id = document.getElementById('id')
+    let order = document.getElementById('order')
     let height = document.getElementById('height')
     let weight  = document.getElementById('weight')
     let base_experience = document.getElementById('base_experience')
@@ -19,28 +20,32 @@ button.addEventListener('click', async () => {
     //Axios call goes here
     //remember to use Async and Await!
     //DOM Setters go here
-    const pokemon = await axios.get(`https://pokeapi.co/api/v2/pokemon/${textInput}`)
+    const pokemonRes = await axios.get(`https://pokeapi.co/api/v2/pokemon/${textInput}`)
+    let pokemon = pokemonRes.data
 
     //Name
-    pokemonName.innerText = pokemon.data.name
+    pokemonName.innerText = pokemon.name.toUpperCase()
     
     //Picture
-    let pic1 = `<img src="${pokemon.data.sprites.front_default}"/>`
-    let pic2 = `<img src="${pokemon.data.sprites.front_shiny}"/>`
-    let pic3 = `<img src="${pokemon.data.sprites.back_default}"/>`
-    let pic4 = `<img src="${pokemon.data.sprites.back_shiny}"/>`
+    let pic1 = `<img src="${pokemon.sprites.front_default}"/>`
+    let pic2 = `<img src="${pokemon.sprites.front_shiny}"/>`
+    let pic3 = `<img src="${pokemon.sprites.back_default}"/>`
+    let pic4 = `<img src="${pokemon.sprites.back_shiny}"/>`
     pokemonImage.innerHTML = pic1 + pic2 + pic3 + pic4
+    homeImage.src = `${pokemon.sprites.other.home.front_default}`
     
     //Basic info
-    id.innerText = `ID: ${pokemon.data.id}`
-    name.innerText = `Name: ${pokemon.data.name}`
-    height.innerText = `Height: ${pokemon.data.height}`
-    weight.innerText = `Weight: ${pokemon.data.weight}`
-    
-    base_experience = `Base Experience: ${pokemon.data.base_experience}`
+    console.log(pokemon.order)
+    id.innerHTML = `ID: <span>${pokemon.id}</span>`
+    name.innerHTML = `Name: <span>${pokemon.name}</span>`
+    order.innerHTML = `Order: <span>${pokemon.order}</span>`
+    console.log(order)
+    height.innerHTML = `Height:  <span>${pokemon.height} </span>`
+    weight.innerHTML = `Weight:  <span>${pokemon.weight} </span>`
+    base_experience.innerHTML = `Base Experience:  <span>${pokemon.base_experience} </span>`
     
     //Abilities
-    let abilitiesData= pokemon.data.abilities
+    let abilitiesData= pokemon.abilities
     let aTitle= `<p>Ability(ies):</p>`
     let aList = ``
     abilitiesData.forEach( (a) => {
@@ -49,12 +54,13 @@ button.addEventListener('click', async () => {
     pokemonAbility.innerHTML = aTitle + aList
 
     //moves
-    let moveData= pokemon.data.moves
+    let moveData= pokemon.moves
     let mTitle= `<p>Move(s):</p>`
     let mList = ``
-    moveData.forEach( (m) => {
-        mList += `<li>${m.move.name}</li>`
-    })
+    for (let i = 0; i < 10; i++) {
+        const move = moveData[i].move.name
+        mList += `<li>${move}</li>`
+    }
     pokemonMove.innerHTML = mTitle + mList
 }
 )
